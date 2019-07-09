ddbf898a4756f3fdb42bae26f5bc45c310e1c482
Marvin Colgin <bpadmin@builtbp.com>
2019-07-09 15:04:22 -0700

    ADD: function to just place the value in the middle of the array

--- challenges/array_shift/array_shift.py
+++ challenges/array_shift/array_shift.py
@@ -1,8 +1,8 @@
-"""
-In the docs, the requirements of this function is to place the "val" in the literal middle of the arrray
-However, the two example use sorted arrays, and I felt the need to go further
-"""
-def insert_shift_array(arr, val):
+def insert_shift_array_sorted(arr, val):
+   """
+   This function inserts a value in a sorted array, at it's numericly associated index
+   """
+
    new = []
 
    # Find location to insert
@@ -28,9 +28,26 @@ def insert_shift_array(arr, val):
    return new
 
 
-arr = [1,2,4,6,7]
-arr = insert_shift_array(arr, 5)
-print(arr)
+
+def insert_shift_array(arr, val):
+    """
+    Insert value in the middle of an array
+    """
+
+    new = []
+
+    c = len(arr) - (len(arr) // 2)
+
+    new.extend(arr[:c])
+    new.append(val)
+    new.extend(arr[c:])
+
+    return new
+
+
+#arr = [1,2,4,6,7]
+#arr = insert_shift_array(arr, 5)
+#print(arr)
 
 #arr = [1,3,4,7,9]
 #arr = isa2(arr, 2)
