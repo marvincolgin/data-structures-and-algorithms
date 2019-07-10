b7b37ee4b29bbf967b47d80b4e05460769a4a106
Marvin Colgin <bpadmin@builtbp.com>
2019-07-10 16:19:36 -0700

    COMPLETE

--- challenges/array_binary_search/array_binary_search.py
+++ challenges/array_binary_search/array_binary_search.py
@@ -1,1 +1,23 @@
+def array_binary_search(arr, val):
 
+    start = 0
+    end = len(arr)-1
+
+    found = False
+    limiter = 0  # THIS ISN'T NEEDED, BUT WHO LIKES INFINITE LOOPS?!?!?
+    while start <= end or limiter > len(arr):
+        mid = ((end - start) // 2) + start
+        if arr[mid] == val:
+            found = True
+            break
+        elif val < arr[mid]:
+            end = mid-1
+        elif val > arr[mid]:
+            start = mid+1
+
+        limiter += 1
+
+    if found:
+        return mid
+    else:
+        return -1
--- challenges/array_binary_search/test_array_binary_search.py
+++ challenges/array_binary_search/test_array_binary_search.py
@@ -1,1 +1,29 @@
-sd
+from array_binary_search import array_binary_search
+
+
+def test_function_exists():
+    assert array_binary_search
+
+
+def test_search_exists_firstidx():
+    expected = 1
+    actual = array_binary_search([1, 3, 5, 7, 9], 3)
+    assert expected == actual
+
+
+def test_search_exists2():
+    expected = 3
+    actual = array_binary_search([1, 3, 5, 7, 9], 7)
+    assert expected == actual
+
+
+def test_search_exists_lastidx():
+    expected = 4
+    actual = array_binary_search([1, 3, 5, 7, 9], 9)
+    assert expected == actual
+
+
+def test_search_noexists():
+    expected = -1
+    actual = array_binary_search([1, 2, 4, 7, 9], 5)
+    assert expected == actual
