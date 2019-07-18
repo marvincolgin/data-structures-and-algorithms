677d9a461991b497a2b218bd27ca257e9305d24f
Marvin Colgin <bpadmin@builtbp.com>
2019-07-18 16:40:59 -0700

    ADD: Stack datastruct and TESTS

--- data-structures/stacks_and_queues/stacks_and_queues.py
+++ data-structures/stacks_and_queues/stacks_and_queues.py
@@ -1,1 +1,33 @@
+import sys
+sys.path.insert(0, '../linked_list')
+from link_list import LinkList
 
+
+# ********************************
+class Stack():
+
+    _data = None
+
+    def __init__(self):
+        self._data = LinkList()
+        pass
+
+    def count(self) -> int:
+        return self._data.count()
+
+    def pop(self) -> str:
+        val = self.peek()
+        if val != '':
+            self._data.remove(val)
+        return val
+
+    def push(self, val: str) -> bool:
+        self._data.insert(val)
+        return True
+
+    def peek(self) -> str:
+        # not going to do it...
+        # return self._data and self.data.head
+        if self._data.head is not None:
+            return self._data.head.value
+        return ''
--- data-structures/stacks_and_queues/test_stacks_and_queues.py
+++ data-structures/stacks_and_queues/test_stacks_and_queues.py
@@ -1,34 +1,75 @@
 import pytest
+import sys
+from stacks_and_queues import Stack
 
 
-def test_stack_push():
-    # STACK: push onto a stack
-    pass
-
-
-def test_stack_push_lots():
-    # STACK: push multiple values onto a stack
-    pass
-
-
-def test_stack_pop():
-    # STACK: pop off the stack
-    pass
-
-
-def test_stack_empty_after_pops():
-    # STACK: empty a stack after multiple pops
-    pass
-
-
-def test_stack_peek():
-    # STACK: peek the next item on the stack
-    pass
+def helper_stack_addlots():
+    stack = Stack()
+    stack.push('pao de queijo')
+    stack.push('pappa sandwidh')
+    stack.push('banana')
+    return stack
 
 
 def test_stack_cancreate():
     # STACK: instantiate an empty stack
-    pass
+    stack = Stack()
+    expected = True
+    actual = isinstance(stack, Stack)
+    assert expected == actual
+
+
+def test_stack_classexists():
+    assert Stack()
+
+
+def test_stack_push():
+    # STACK: push onto a stack
+    stack = Stack()
+    stack.push('pao de queijo')
+    actual = stack.peek()
+    expected = 'pao de queijo'
+    assert actual == expected
+
+
+def test_stack_push_lots():
+    # STACK: push multiple values onto a stack
+    stack = helper_stack_addlots()
+    expected = 3
+    actual = stack.count()
+    assert expected == actual
+
+
+def test_stack_pop():
+    # STACK: pop off the stack
+    stack = helper_stack_addlots()
+    expected = 'banana'
+    actual = stack.pop()
+    assert expected == actual
+    assert stack.count() == 2
+
+
+def test_stack_empty_after_pops():
+    # STACK: empty a stack after multiple pops
+    stack = helper_stack_addlots()
+    for x in range(stack.count()-1):
+        stack.pop()
+    expected = 1
+    actual = stack.count()
+    assert expected == actual
+
+    stack.pop()
+    actual = stack.count()
+    expected = 0
+    assert expected == actual
+
+
+def test_stack_peek():
+    # STACK: peek the next item on the stack
+    stack = helper_stack_addlots()
+    expected = 'banana'
+    actual = stack.peek()
+    assert actual == expected
 
 
     """
