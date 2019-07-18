767989a11808b7ade6d9c75bff0d94785d799550
Marvin Colgin <bpadmin@builtbp.com>
2019-07-18 15:40:53 -0700

    ADD: init framework for tests.

---
+++ data-structures/stacks_and_queues/test_stacks_and_queues.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+def test_stack_push():
+    # STACK: push onto a stack
+    pass
+
+
+def test_stack_push_lots():
+    # STACK: push multiple values onto a stack
+    pass
+
+
+def test_stack_pop():
+    # STACK: pop off the stack
+    pass
+
+
+def test_stack_empty_after_pops():
+    # STACK: empty a stack after multiple pops
+    pass
+
+
+def test_stack_peek():
+    # STACK: peek the next item on the stack
+    pass
+
+
+def test_stack_cancreate():
+    # STACK: instantiate an empty stack
+    pass
+
+
+    """
+* QUEUE: enqueue into a queue
+* QUEUE: enqueue multiple values into a queue
+* QUEUE: dequeue out of a queue the expected value
+* QUEUE: peek into a queue, seeing the expected value
+* QUEUE: empty a queue after multiple dequeues
+* QUEUE: instantiate an empty queue
+    """
