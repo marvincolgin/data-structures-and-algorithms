fdc77c37db6d1d3f0163eea46161e8c484600d0d
Marvin Colgin <bpadmin@builtbp.com>
2019-07-23 20:55:24 -0700

    TESTS: class Animal

--- challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -6,27 +6,41 @@ p = parentdir+'\\..\\data-structures\\linked_list'
 sys.path.insert(0,p)
 sys.path.insert(0,p)
 from stacks_and_queues import Queue
-from enum import Enum
+from enum import IntEnum
+import json
 
 
-class AnimalType(Enum):
+class AnimalType(IntEnum):
+    UNKNOWN = 0
     CAT = 1
     DOG = 2
 
+
 class Animal(object):
+
+    type = AnimalType.UNKNOWN
 
     def __init__(self, type : AnimalType):
         # create obj
-        pass
+        self.type = type
 
     def serialize(self):
         # return json for obj
-        pass
+        return json.dumps(self.__dict__)
 
     @staticmethod
-    def Factory(self, json : str): # -> Animal
+    def Factory(jsonstr : str): # -> Animal
         # create Animal class Dog|Cat for Json
-        pass
+        d = json.loads(jsonstr)
+        print(f'd:[{d}]')
+        if d['type'] == AnimalType.CAT:
+            obj = Animal(AnimalType.CAT)
+        elif d['type'] == AnimalType.DOG:
+            obj = Animal(AnimalType.DOG)
+        else:
+            print('Bad JSON')
+            return None
+        return obj
 
 
 class Cat(Animal):
--- challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
@@ -6,3 +6,16 @@ def test_class_exists():
     Cat()
     Dog()
 
+def test_animal_serialize():
+    a = Animal(AnimalType.CAT)
+    actual = a.serialize()
+    expected = '{"type": 1}'
+    assert actual == expected
+
+def test_animal_factory():
+    serialdata = '{"type": 1}'
+    a = Animal.Factory(serialdata)
+    actual = a.serialize()
+    expected = serialdata
+    assert actual == expected
+
