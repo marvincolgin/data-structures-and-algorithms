f3e53edfdd109b1e670785abf8b43bfff69b9eb0
Marvin Colgin <bpadmin@builtbp.com>
2019-07-26 16:17:58 -0700

    FINISH: documenting api and readme

--- data-structures/tree/tree.py
+++ data-structures/tree/tree.py
@@ -1,14 +1,17 @@
 from enum import IntEnum
 
 class TraverseMethod(IntEnum):
+    # enum class for traversal and processing order
     IN_ORDER = 1
     PRE_ORDER = 2
     POST_ORDER = 3
 
 
 class Node:
+    # class for nodes within Tree
 
     def __init__(self, value):
+        # constructor for creating Node
         self.value = value
         self.left = None
         self.right = None
@@ -17,11 +20,14 @@ class Node:
 class BinaryTree:
 
     def __init__(self):
+        # constructor for creating BinaryTree
         self.root = None
 
-    def traverse(self, method, action_func):
+    def traverse(self, method : TraverseMethod, action_func):
+        # visit each node in atree, using a specified method and call action_func() for each node
 
         def _visit(node):
+            # recusive function for visiting each node
 
             if method == TraverseMethod.PRE_ORDER:
                 action_func(node.value)
@@ -40,7 +46,8 @@ class BinaryTree:
 
         _visit(self.root)
 
-    def returnAsArr(self, method):
+    def returnAsArr(self, method : TraverseMethod):
+        # return the enter tree as an array using a specified method
 
         result = []
 
@@ -52,11 +59,14 @@ class BinaryTree:
 
 
 class BinarySearchTree(BinaryTree):
+    # class for binary-search-tree
 
     def add(self, new_value):
         # adds new value to the tree
 
         def _find_and_insert(node):
+            # recursive method for evaluating a node and calling itself depending on the value
+
             if node.value > new_value:
                 if node.left is None:
                     node.left = Node(new_value)
@@ -77,6 +87,8 @@ class BinarySearchTree(BinaryTree):
         # accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 
         def _visit(node):
+            # recursive function for isiting each node
+
             if node.value == target_value:
                 return True
             if node.value > target_value:
